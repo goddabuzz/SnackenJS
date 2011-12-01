9a448c2a12dc9500477dc585aa4cf5c5983bf78b
Daniël Kloosterman <buz.i286@gmail.com>
2011-12-01 15:35:33 +0100

    Menu update
    Signed-off-by: Daniël Kloosterman <buz.i286@gmail.com>

--- data/menu.js
+++ data/menu.js
@@ -1,6 +1,6 @@
 module.exports = {
     menuItems: [
-        { id:    1    ,	 "name": "	Patat Pinda	", "price":	1.8	},
+    { id:   1   ,	 "name": "	Patat Pinda	", "price":	1.8	},
     { id:	2	,	 "name": "	Patat Speciaal	", "price":	1.8	},
     { id:	3	,	 "name": "	Patat Mayo	", "price":	1.65	},
     { id:	4	,	 "name": "	Patat (Zonder Saus)	", "price":	1.4	},
@@ -17,6 +17,30 @@ module.exports = {
     { id:	15	,	 "name": "	Broodje Bamibal	", "price":	1.95	},
     { id:	16	,	 "name": "	Broodje Tartaar Speciaal (Zonder Curry)	", "price":	2.5	},
     { id:	17	,	 "name": "	Stokbrood Gezond	", "price":	2.75	},
-    { id:	18	,	 "name": "	Stokbrood Warm Vlees	", "price":	3.5	}
+    { id:	18	,	 "name": "	Stokbrood Warm Vlees	", "price":	3.5	},
+	
+	
+	{ id:	19	,	 "name": "	Patat Oorlog	", "price":	1.9	},
+	{ id:	20	,	 "name": "	Patat Knoflooksaus	", "price":	1.9	},
+	{ id:	21	,	 "name": "	Patat Sambal	", "price":	1.65	},
+	{ id:	22	,	 "name": "	Patat Curry	", "price":	1.65	},
+	{ id:	23	,	 "name": "	Patat Ketchup	", "price":	1.65	},
+	{ id:	24	,	 "name": "	Patat Whiskeysaus	", "price":	1.9	},
+	{ id:	25	,	 "name": "	Patat Ravigottesaus	", "price":	1.9	},	
+	{ id:	26	,	 "name": "	Vleeskroket	", "price":	1.4	},
+	{ id:	27	,	 "name": "	Goulashkroket	", "price":	1.4	},
+	{ id:	28	,	 "name": "	Satekroket	", "price":	1.4	},
+	{ id:	29	,	 "name": "	Eierbal	", "price":	1.5 },
+	{ id:	30	,	 "name": "	Mexicano	", "price":	1.6 },
+	{ id:	31	,	 "name": "	Sweet Meat	", "price":	1.6 },
+	{ id:	32	,	 "name": "	Pikanto	", "price":	1.6 },	
+	{ id:	33	,	 "name": "	Kipcorn	", "price":	1.6 },
+	{ id:	34	,	 "name": "	Kipnuggets, 5 stuks	", "price":	1.75 },
+	{ id:	35	,	 "name": "	Shoarmarol	", "price":	1.8 },
+	{ id:	36	,	 "name": "	Halve Haan	", "price":	3.4 }
+	
+	
+	
+	
     ]
 };
